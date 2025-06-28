cafdf84e3eb1e05db0ff7d594b6bfa0d0134e160
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2025-06-29 01:05:56 +0530

    Update setup_playwright.py

--- setup_playwright.py
+++ setup_playwright.py
@@ -1,14 +1,23 @@
 import os
 import subprocess
+import streamlit
+from playwright.sync_api import sync_playwright
+
 
 def install_playwright():
-    try:
-        # Install playwright and browsers (Chromium)
-        subprocess.run(["pip", "install", "playwright"], check=True)
-        subprocess.run(["playwright", "install", "chrome"], check=True)
-        print("Playwright and Chromium installed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error during installation: {e}")
+    with sync_playwright() as p:
+        try:
+            p.chromium.install()
+       
+            # # Install playwright and browsers (Chromium)
+            # subprocess.run(["pip", "install", "playwright"], check=True)
+            # subprocess.run(["playwright", "install", "chrome"], check=True)
+           
+           
+            # print("Playwright and Chromium installed successfully.")
+            st.write('Playwright and Chromium installed successfully')
+        except subprocess.CalledProcessError as e:
+            st.write(f"Error during installation: {e}")
 
 if __name__ == "__main__":
     install_playwright()
